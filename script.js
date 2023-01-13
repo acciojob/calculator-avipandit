d2578ff223fabdcb3320d1c53dcaf388108d9daa
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-01-13 14:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your code here
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("input").value = "";
+    }
+
+    // This function display values
+    function display(value) {
+    document.getElementById("input").value += value;
+    }
+
+    // This function evaluates the expression and returns result
+    function calculate() {
+    var p = document.getElementById("input").value;
+    var q = eval(p);
+    document.getElementById("input").value = q;
+    }
